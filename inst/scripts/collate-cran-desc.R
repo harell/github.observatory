3e82a76a551888c44432b19a1e192421ad7f66fa
harell <harel@loostiger.com>
2022-02-05 12:27:08 +1300

    split scripts

---
+++ inst/scripts/collate-cran-desc.R
@@ -0,0 +1,59 @@
+# Setup -------------------------------------------------------------------
+pkgload::load_all(usethis::proj_get(), quiet = TRUE)
+repository <- Repository$new()
+
+
+# Description -------------------------------------------------------------
+pkg_desc <- tools::CRAN_package_db()
+
+invisible(
+    tidy_pkg_desc <- pkg_desc
+    |> standardise_col_names()
+    |> dplyr::rowwise()
+    |> dplyr::transmute(
+        package = as.character(package),
+        github_slug = dplyr::if_else(
+            github$is_valid_url(bug_reports),
+            github$compose$slug(owner = github$extract$owner(bug_reports), repo = package),
+            github$compose$slug(owner = "cran", repo = package)
+        )
+    )
+    |> dplyr::ungroup()
+)
+
+repository$write_pkg_desc(tidy_pkg_desc)
+
+
+# Teardown ----------------------------------------------------------------
+repository$commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ inst/scripts/parse-repo-desc.R
@@ -0,0 +1,64 @@
+# Setup -------------------------------------------------------------------
+pkgload::load_all(usethis::proj_get(), quiet = TRUE)
+repository <- Repository$new()
+archive <- Archive$new()
+
+
+# Parse Pacakges Information ----------------------------------------------
+tags <- archive$show() |> dplyr::filter(type %in% "stargazers")
+artifacts <- archive$load(tags$artifact)
+names(artifacts) <- tags$repo
+
+pb <- progress::progress_bar$new(format = "Parsing Stargazers [:bar] :current/:total (:percent) eta: :eta", total = length(tags$repo), clear = FALSE)
+entries <- tibble::tibble()
+for(repo in tags$repo){
+    pb$tick(1)
+    invisible(
+        new_entry <- artifacts
+        |> purrr::pluck(repo)
+        |> purrr::map_chr(~purrr::pluck(.x, "login"))
+        |> tibble::enframe("repo", "stargazer")
+        |> dplyr::mutate(repo = !!repo)
+        # |> dplyr::group_by(repo)
+        # |> dplyr::summarise(stargazer = list(stargazer), .groups = "drop")
+    )
+    if(nrow(new_entry) == 0) new_entry <- tibble::tibble(repo = !!repo, stargazer = NULL)
+    entries <- dplyr::bind_rows(entries, new_entry)
+}
+
+entries |> dplyr::n_distinct("stargazer")
+
+
+# Teardown ----------------------------------------------------------------
+archive$clean()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
