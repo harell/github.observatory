560ef01da91edc12eae26ca28be3ee7c5d600abb
harell <harel@loostiger.com>
2022-02-05 11:45:08 +1300

    nest users

--- inst/scripts/collate-packages-data.R
+++ inst/scripts/collate-packages-data.R
@@ -55,10 +55,64 @@ for(package in packages) tryCatch({
 }, error = function(e) pb$message(glue("Failed to get {package} information")))
 
 ## Process Packages Stats
-archive$show()
+withr::with_seed(
+    814,
+    tags <- archive$show()
+    |> dplyr::filter(type %in% "stargazers")
+)
 
-artifact <- archive$load()
+artifacts <- archive$load(tags$artifact)
+names(artifacts) <- tags$repo
+
+pb <- progress::progress_bar$new(format = "Parsing Stargazers [:bar] :current/:total (:percent) eta: :eta", total = length(tags$repo), clear = FALSE)
+entries <- tibble::tibble()
+for(repo in tags$repo){
+    pb$tick(1)
+    invisible(
+        new_entry <- artifacts
+        |> purrr::pluck(repo)
+        |> purrr::map_chr(~purrr::pluck(.x, "login"))
+        |> tibble::enframe("repo", "stargazers")
+        |> dplyr::mutate(repo = !!repo)
+        |> dplyr::group_by(repo)
+        |> dplyr::summarise(stargazers = list(stargazers), .groups = "drop")
+    )
+    if(nrow(new_entry) == 0) new_entry <- tibble::tibble(repo = !!repo, stargazers = NULL)
+    entries <- dplyr::bind_rows(entries, new_entry)
+}
+
 
 # Teardown ----------------------------------------------------------------
 archive$clean()
 repository$commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
