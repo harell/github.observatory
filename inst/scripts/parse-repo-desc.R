3e28b27e2b6f1a1f74f70fd03e752dad9c859101
harell <harel@loostiger.com>
2022-02-06 19:27:39 +1300

    cast stargazers id as integer

--- inst/scripts/parse-repo-desc.R
+++ inst/scripts/parse-repo-desc.R
@@ -23,11 +23,11 @@ for(repo in tags$repo) tryCatch({
     invisible(
         stargazers <- artifacts
         |> purrr::pluck(repo)
-        |> purrr::map_chr(~purrr::pluck(.x, "id") |> as.integer())
+        |> purrr::map_chr(~purrr::pluck(.x, "id"))
         |> tibble::enframe("repo", "stargazer")
         |> dplyr::mutate(repo = !!repo)
         |> dplyr::group_by(repo)
-        |> dplyr::summarise(stargazers = list(stargazer), .groups = "drop")
+        |> dplyr::summarise(stargazers = list(as.integer(stargazer)), .groups = "drop")
         |> dplyr::pull(stargazers)
     )
     if(length(stargazers) == 0) next
