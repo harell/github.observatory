2b9f06c53be75f10f63ca9de7e971fc06ee8b8de
harell <harel@loostiger.com>
2022-02-07 18:28:14 +1300

    get user data

--- inst/scripts/collate-user-info.R
+++ inst/scripts/collate-user-info.R
@@ -14,21 +14,22 @@ invisible(
 )
 
 pb <- progress::progress_bar$new(format = "Quering Github Users [:bar] :current/:total (:percent) eta: :eta", total = length(users), clear = FALSE)
-for(user in users) tryCatch({
     if((which(users %in% user) - 1) %% 10 == 0) if(github$return_remaining_quote() < 50) {pb$message(glue("Reached GitHub API call limit")); break}
     # if((which(users %in% user) - 1) %% 10 == 0) while(github$return_remaining_quote() < 50) Sys.sleep(60)
+for(user in users) tryCatch({
     try(pb$tick(1), silent = TRUE)
 
     artifact <- query$user$by_id(user)
-    archive$save(artifact, tags = c("entity:user", "type:overview", paste0("id:", user_id)))
+    archive$save(artifact, tags = c("entity:user", "type:overview", paste0("id:", user)))
 
-    artifact <- query$user$following(artifact$login)
+    artifact <- ifelse(artifact$following == 0, list(), query$user$following(artifact$login))
     artifact <- artifact |> purrr::map(~purrr::keep(.x, names(.x) %in% c("login", "id")))
     archive$save(artifact, tags = c("entity:user", "type:following", paste0("id:", user)))
 
     try(pb$message(glue("Retrieved user `{user}`")), silent = TRUE)
-}, error = function(e) try(pb$message(glue("Failed to Retrieve user `{user_id}`")), silent = TRUE))
+}, error = function(e) try(pb$message(glue("Failed to Retrieve user `{user}`")), silent = TRUE))
 
 
 # Teardown ----------------------------------------------------------------
+archive$clean()
 # repository$write_repo_desc(tidy_repo_desc)$commit()
