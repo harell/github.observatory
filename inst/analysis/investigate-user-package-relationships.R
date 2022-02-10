f1d1a52f0524c1429e301283dd3a8cce7e077a20
harell <harel@loostiger.com>
2022-02-10 21:07:13 +1300

    create sparse matirx

--- inst/analysis/investigate-user-package-relationships.R
+++ inst/analysis/investigate-user-package-relationships.R
@@ -5,9 +5,19 @@ repository <- Repository$new()
 
 
 # Compose Stargazer-Package Matrix ----------------------------------------
-(
+invisible(
     repo_desc <- repository$read_repo_desc()
     |> tidyr::unnest(stargazers)
+    |> dplyr::rename(stargazer_id = stargazers)
     |> dplyr::distinct()
 )
 
+r_verse <- Matrix::sparseMatrix(repo_desc$stargazer_id, repo_desc$repo_id)
+
+
+# Developer Zone ----------------------------------------------------------
+data("MovieLense", package = "recommenderlab")
+class(MovieLense)
+recommenderlab::getRatingMatrix()
+
+recommenderlab::dropNA()
