4cd4c7a8653b3100571c5dde438b0ed85d7e8843
harell <harel@loostiger.com>
2022-02-12 17:17:22 +1300

    prepare data

--- inst/analysis/investigate-user-package-relationships.R
+++ inst/analysis/investigate-user-package-relationships.R
@@ -1,5 +1,5 @@
 # Setup -------------------------------------------------------------------
-# remotes::install_cran("recommenderlab")
+# remotes::install_cran(c("recommenderlab", "arules"), quiet = TRUE)
 pkgload::load_all(usethis::proj_get(), quiet = TRUE)
 repository <- Repository$new()
 
@@ -12,7 +12,25 @@ invisible(
     |> dplyr::distinct()
 )
 
-r_verse <- Matrix::sparseMatrix(repo_desc$stargazer_id, repo_desc$repo_id)
+(
+    user_pkg_matrix <- repo_desc
+    # |> dplyr::sample_n(1e3)
+    |> dplyr::select(stargazer_id, repo_id)
+    |> dplyr::transmute(user = stargazer_id, package = repo_id)
+    |> purrr::modify(factor)
+    |> as("transactions")
+    |> as("itemMatrix")
+    |> as("binaryRatingMatrix")
+)
+
+
+# Statistics --------------------------------------------------------------
+a <- recommenderlab::rowCounts(user_pkg_matrix)
+
+
+# Model data --------------------------------------------------------------
+
+
 
 
 # Developer Zone ----------------------------------------------------------
