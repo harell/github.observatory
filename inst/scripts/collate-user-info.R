fe6a0f27cfd3562528680fd95855627a4f4b26a5
harell <harel@loostiger.com>
2022-03-06 18:27:37 +1300

    update

--- inst/scripts/collate-user-info.R
+++ inst/scripts/collate-user-info.R
@@ -32,7 +32,7 @@ users <- setdiff(spectators, tryCatch(user_archive$show() |> dplyr::filter(entit
 pb <- progress::progress_bar$new(format = "Quering Github Users [:bar] :current/:total (:percent) eta: :eta", total = length(users), clear = FALSE)
 for(user in users){try(pb$tick(1), silent = TRUE); tryCatch({
     github$alter_PAT()
-    if((which(users %in% user) - 1) %% 10 == 0) while(github$return_remaining_quote() < 50) Sys.sleep(60)
+    if((which(users %in% user) - 1) %% 10 == 0) while(github$return_remaining_quote() < 10) Sys.sleep(60)
 
     user_overview <- query$user$by_id(user)
     user_archive$save(user_overview, tags = c("entity:user", "type:overview", paste0("id:", user)))
