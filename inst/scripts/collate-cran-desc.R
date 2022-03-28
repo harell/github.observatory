1331e43513c1c5050fc9d4495cbc94a9bfb8caeb
harell <harel@loostiger.com>
2022-03-28 19:44:41 +1300

    fix packages naming mismatches

--- inst/scripts/collate-cran-desc.R
+++ inst/scripts/collate-cran-desc.R
@@ -21,6 +21,8 @@ invisible(
     |> dplyr::distinct(package, .keep_all = TRUE)
 )
 
+
+# Process data ------------------------------------------------------------
 ## Create Bijection: One-to-One Relationship between 'package' and 'full_name'
 invisible(
     tidy_packages <- packages
@@ -33,6 +35,14 @@ invisible(
     |> dplyr::mutate(full_name = dplyr::if_else(index == 1, full_name, github$compose$slug(owner = "cran", repo = package)))
     |> dplyr::select(-n, -n_char, -index)
     |> dplyr::arrange(package)
+)
+
+## Format repo names to have packages names
+invisible(
+    tidy_packages <- tidy_packages
+    |> dplyr::mutate(owner = github$extract$owner(full_name), repo = github$extract$repo(full_name))
+    |> dplyr::mutate(full_name = dplyr::if_else(tolower(package) == tolower(repo), github$compose$slug(owner, package), full_name))
+    |> dplyr::select(-owner, -repo)
 )
 
 
