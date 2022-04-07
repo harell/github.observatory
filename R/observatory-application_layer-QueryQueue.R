73b1769c8753714df8625cf640832830ef511f5d
harell <harell@users.noreply.github.com>
2022-04-07 20:55:50 +1200

    return collections::priority_queue

--- R/observatory-application_layer-QueryQueue.R
+++ R/observatory-application_layer-QueryQueue.R
@@ -8,10 +8,18 @@ QueryQueue <- R6::R6Class(
     ), private = list(
         # Private Fields ----------------------------------------------------------
         depo = new.env(),
-        null_query = c()
+        null_query = c(),
         # Private Methods ---------------------------------------------------------
+        generate_REPO_queue = function() collections::priority_queue(),
+        generate_USER_queue = function() collections::priority_queue()
     ), active = list(
-        REPO = function() {return(new.env())},
-        USER = function() {return(new.env())}
+        REPO = function() private$generate_REPO_queue(),
+        USER = function() private$generate_USER_queue()
     )
 )
+
+
+# Class Methods -----------------------------------------------------------
+# QueryQueue$set(which = "private", name = "generate_REPO_queue", overwrite = TRUE, value = function() {
+#     collections::priority_queue
+# })
--- tests/testthat/test-QueryQueue.R
+++ tests/testthat/test-QueryQueue.R
@@ -3,8 +3,8 @@ test_that("QueryQueue works", {
     expect_type(queue$REPO, "environment")
     expect_type(queue$USER, "environment")
 
-    # expect_s3_class(depo$read_REPO(), "data.frame")
-    # expect_s3_class(depo$read_PACKAGE(), "data.frame")
-    # expect_s3_class(depo$read_FOLLOWING(), "data.frame")
-    # expect_s3_class(depo$read_SPECTATOR(), "data.frame")
+})
+
+test_that("QueryQueue generates queue object", {
+    expect_gte(QueryQueue$new()$REPO$size(), 0)
 })
