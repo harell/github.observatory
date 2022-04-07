6beb9f8ba86f512c9006e16be863f5fd4dc0a942
harell <harell@users.noreply.github.com>
2022-04-07 20:46:13 +1200

    add active variables

--- tests/testthat/test-QueryQueue.R
+++ tests/testthat/test-QueryQueue.R
@@ -1,6 +1,8 @@
 test_that("QueryQueue works", {
-    expect_s3_class(depo <- QueryQueue$new(), "R6")
-    # expect_s3_class(depo$read_USER(), "data.frame")
+    expect_s3_class(queue <- QueryQueue$new(), "R6")
+    expect_type(queue$REPO, "environment")
+    expect_type(queue$USER, "environment")
+
     # expect_s3_class(depo$read_REPO(), "data.frame")
     # expect_s3_class(depo$read_PACKAGE(), "data.frame")
     # expect_s3_class(depo$read_FOLLOWING(), "data.frame")
