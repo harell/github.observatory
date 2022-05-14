6bbe6720d05dadbc323ce1f19b0b674de0fa4f01
harell <harell@users.noreply.github.com>
2022-05-14 17:15:54 +1200

    save skeleton

--- R/QueryDB.R
+++ R/QueryDB.R
@@ -1,18 +1,36 @@
 #' @title Database for Queries
 #'
 #' @export
-QueryDB <- R6::R6Class(cloneable = FALSE, public = list(
+QueryDB <- R6::R6Class(class = "QueryDB", cloneable = FALSE, public = list(
     # Public Methods ----------------------------------------------------------
+    #' @description Instantiate a QueryDB object
     #' @param path (`character`) A character denoting an existing directory of the Archive for which metadata will be aved and returned.
     #' @param immediate (`logical`) Should queries be committed immediately?
     initialize = function(path, immediate = FALSE){
         fs::dir_create(path)
         private$path <- path
         private$immediate <- immediate
+    },
+    #' @description Save a query in the database
+    #' @param data (`list`) The query data.
+    #' @param entity (`character`) The entity name. For example "repo" or "user".
+    #' @param type (`character`) The query type. For example "overview" or "contributors".
+    #' @param id (`character`) The entity id. For example "280924484" or "clintools".
+    save = function(data, entity, type, id){
+        private$.save(data, entity, type, id)
+        invisible(self)
     }
 ), private = list(
     # Private Fields ----------------------------------------------------------
     path = character(),
-    immediate = logical()
+    immediate = logical(),
     # Private Methods ---------------------------------------------------------
+    .save = function(data, entity, type, id) { stop() }
 ))
+
+
+# Private Methods ---------------------------------------------------------
+QueryDB$set("private", ".save", overwrite = TRUE, value = function(data, entity, type, id){
+    invisible()
+})
+
--- tests/testthat/test-QueryDB.R
+++ tests/testthat/test-QueryDB.R
@@ -1,8 +1,26 @@
 # Setup -------------------------------------------------------------------
 path <- fs::file_temp("QueryDB")
+repo_query <- list(
+    id = 280924484,
+    name = "clintools",
+    owner = list(login = "lilleoel", id = 68481897),
+    stargazers_count = 1
+)
 
 
 # Tests -------------------------------------------------------------------
 test_that("QueryDB constructor works", {
-  expect_s3_class(QueryDB$new(path, immediate = FALSE), "R6")
+  expect_s3_class(query_db <<- QueryDB$new(path, immediate = FALSE), "R6")
 })
+
+
+# Save and Load -----------------------------------------------------------
+test_that("QueryDB save() works", {
+    expect_s3_class(query_db$save(
+        data = repo_query,
+        entity = "repo",
+        type = "overview",
+        id = repo_query$id
+    ), "R6")
+})
+
