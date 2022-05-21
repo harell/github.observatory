8b5a977a4beb3c3a03964ad8285653f9eed5d36a
harell <harell@users.noreply.github.com>
2022-05-21 19:50:39 +1200

    adjust parse-user-info.R

--- inst/scripts/parse-user-info.R
+++ inst/scripts/parse-user-info.R
@@ -1,12 +1,11 @@
 # Setup -------------------------------------------------------------------
 pkgload::load_all(usethis::proj_get(), quiet = TRUE)
-if(does_not_exist("user_archive")) user_archive <- UserArchive$new()
-if(does_not_exist("depo")) depo <- Depository$new()
+if(does_not_exist("user_db")) user_db <- UserQueryDB$new()
 
 
 # Load cached data --------------------------------------------------------
 invisible(
-    artifacts <- user_archive$show()
+    queries <- user_db$load()
     |> dplyr::filter(type %in% "overview")
     |> dplyr::arrange(dplyr::desc(date))
     |> dplyr::distinct(id, .keep_all = TRUE)
@@ -15,28 +14,32 @@ invisible(
 
 # Parse users -------------------------------------------------------------
 invisible(
-    messy_users <- artifacts$artifact
-    |> purrr::map_dfr(~.x |> user_archive$load() |> unlist())
+    users_info <- queries
+    |> dplyr::rename(user_id = id)
+    |> dplyr::rowwise()
+    |> dplyr::pull(data)
+    |> purrr::map_dfr(~ .x |> jsonlite::fromJSON() |> unlist())
+    |> observatory$standardise$col_names()
     |> dplyr::transmute(
         id           = as.integer(id),
         login        = as.character(login),
         avatar_url   = as.character(avatar_url),
         html_url     = as.character(html_url),
-        name         = as.character(name %||% NA_character_) |> observatory$standardise$name(),
+        name         = dplyr::if_else(is.na(name) | is.null(name), "", observatory$standardise$name(name)),
         public_repos = as.integer(public_repos),
         followers    = as.integer(followers),
         following    = as.integer(following),
         created_at   = lubridate::ymd_hms(created_at) |> observatory$standardise$date(),
         updated_at   = lubridate::ymd_hms(updated_at) |> observatory$standardise$date(),
         queried_at   = lubridate::ymd_hms(queried_at) |> observatory$standardise$date(),
-        processed_at = Sys.Date() |> observatory$standardise$date()
+        processed_at = observatory$standardise$date(Sys.Date())
     )
+    |> observatory$discard$ghosts()
+    |> observatory$discard$robots()
 )
 
 invisible(
-    tidy_users <- messy_users
-    |> observatory$discard$ghosts()
-    |> observatory$discard$robots()
+    users_extra_info <- users_info
     |> tibble::add_column(
         r_followers = NA_integer_,
         r_following = NA_integer_,
@@ -49,4 +52,4 @@ invisible(
 
 
 # Teardown ----------------------------------------------------------------
-depo$overwrite_USER(tidy_users)
+depo$overwrite_USER(users_extra_info)
