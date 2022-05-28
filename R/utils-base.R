ca0cf87ec4118b6843367038677d9873ec743da8
harell <harell@users.noreply.github.com>
2022-05-28 19:44:01 +1200

    update documentaion

--- R/utils-base.R
+++ R/utils-base.R
@@ -1,28 +1,28 @@
 #' @name utils-base
 #' @rdname utils-base
-#'
+#' @noRd
 #' @title Base Functions
 #'
 NULL
 
 
 #' @rdname utils-base
-#' @export
+#' @noRd
 does_not_exist <- purrr::negate(base::exists)
 
 
 #' @rdname utils-base
-#' @export
+#' @noRd
 file_not_exists <- purrr::negate(base::file.exists)
 
 
 #' @rdname utils-base
-#' @export
+#' @noRd
 `%not_in%` <- purrr::negate(`%in%`)
 
 
 #' @rdname utils-base
-#' @export
+#' @noRd
 permute <- function(x){
     if(length(x) == 0){
         return(sample(x))
