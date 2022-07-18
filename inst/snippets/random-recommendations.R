f2b337dcb410d9983c64a0321778dadecdbc9d2a
harell <harell@users.noreply.github.com>
2022-07-18 20:29:42 +1200

    correct test

--- inst/snippets/random-recommendations.R
+++ inst/snippets/random-recommendations.R
@@ -14,15 +14,14 @@ null_table <- tibble::tibble(rank = NA_integer_, user_id = NA_integer_)[0,]
 (
     users <- ecos$read_USER()
     |> dplyr::filter(id %not_in% users_to_exclude)
+    |> dplyr::filter(r_followers >= 25)
     |> dplyr::transmute(
         id = id,
-        login = login,
-        r_followers = r_followers,
-        p = logb(r_followers+1, base = 1.5),
+        p = logb(r_followers, base = 2),
         flag = as.logical(rmultinom(n = 1, size = 10, p))
     )
     |> dplyr::filter(flag == TRUE)
-    |> dplyr::arrange(-r_followers)
+    |> dplyr::arrange(-p)
     |> dplyr::transmute(user_id = as.integer(id))
     |> tibble::rowid_to_column(var = "rank")
 )
@@ -31,3 +30,23 @@ null_table <- tibble::tibble(rank = NA_integer_, user_id = NA_integer_)[0,]
 log(rank(users$r_followers, ties.method = "min"))
 
 rmultinom(n, 1, users$p)
+
+
+
+# Random Packages ---------------------------------------------------------
+repos_to_exclude <- integer(0)
+
+(
+    repos <- ecos$read_REPO()
+    |> dplyr::filter(id %not_in% repos_to_exclude)
+    |> dplyr::filter(stargazers_count  >= 81)
+    |> dplyr::transmute(
+        id = id,
+        p = logb(stargazers_count, base = 2),
+        flag = as.logical(rmultinom(n = 1, size = n, p))
+    )
+    |> dplyr::filter(flag == TRUE)
+    |> dplyr::arrange(-p)
+    |> dplyr::transmute(repo_id = as.integer(id))
+    |> tibble::rowid_to_column(var = "rank")
+)
